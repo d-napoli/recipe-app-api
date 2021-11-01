36a57f539f8b6ddc152d93928e4a7181dc2f725d
d-napoli <d.napoli.dev@gmail.com>
2021-11-01 09:06:00 -0300

    Updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,33 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    # each of those items are sections inside of the admin
+    # the _ means that the text can be translated by the gettext
+    # for adding new fields to the admin page, just add them below
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),  # css classes to the frontend admin add page
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
